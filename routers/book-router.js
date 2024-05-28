88a475163b28f12f5c480bbb268f2461be334142
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2024-05-28 18:04:41 +0530

    Created API for update the book and delete the book

--- routers/book-router.js
+++ routers/book-router.js
@@ -2,6 +2,7 @@ let express = require("express");
 const { BookModel } = require("../models/book-model");
 let bookRouter = express.Router();
 
+/** API for get books */
 bookRouter.get("/books", async (req, res) => {
   try {
     let books = await BookModel.find();
@@ -11,10 +12,11 @@ bookRouter.get("/books", async (req, res) => {
       data: books,
     });
   } catch (error) {
-    return res.status(200).send({ error: error.message });
+    return res.status(400).send({ error: error.message });
   }
 });
 
+/** API for create books */
 bookRouter.post("/books/create", async (req, res) => {
   try {
     let { title, author, isbin, publicationDate } = req.body;
@@ -45,7 +47,55 @@ bookRouter.post("/books/create", async (req, res) => {
       data: newBook,
     });
   } catch (error) {
-    console.log(error.message);
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+/** API for update books */
+bookRouter.patch("/books/update", async (req, res) => {
+  try {
+    let { bookId } = req.query;
+    let { title, author, isbin, publicationDate } = req.body;
+
+    let updatedBook = await BookModel.findByIdAndUpdate(
+      { _id: bookId },
+      {
+        title,
+        author,
+        isbin,
+        publicationDate,
+      },
+      { new: true }
+    );
+
+    if (!updatedBook) {
+      return res.status(400).send({ error: "Book not found" });
+    }
+
+    return res.status(200).send({
+      updatedBook,
+    });
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+// API for delete
+bookRouter.delete("/books/delete", async (req, res) => {
+  try {
+    let { bookId } = req.query;
+
+    let deleteBook = await BookModel.findByIdAndDelete({ _id: bookId });
+
+    if (!deleteBook) {
+      return res.status(400).send({ error: "Book not found" });
+    }
+
+    return res.status(200).send({
+      message: "book deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
   }
 });
 
